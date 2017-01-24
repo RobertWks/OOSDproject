90c7a83b8dacd171e10b633c11d00680b1cf38a5
RobertWks <robertwks@hotmail.co.uk>
2017-01-24 12:39:53 +0000

    Add files via upload
    Starting files for the project

---
+++ World.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiestarter;
+
+/**
+ *
+ * @author Owner
+ */
+public class World {
+    private String info;
+    
+    World (String info) {
+        this.info = info;
+    }
+    
+    public String getInfo(){
+        return info;
+    }
+}
